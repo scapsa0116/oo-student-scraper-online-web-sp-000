b00165281e66e7f9972e8a3a679e5d9196516e4b
Stela Capsa <stela.capsa@gmail.com>
2020-01-29 02:27:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,9 +24,16 @@ students_array.each{|student| Student.new(student)}
 # {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"}]
   end
 
-  def add_student_attributes(attributes_hash)
-    binding.pry
+
     attributes_hash.each{|k,v| self.send(("#{k}="),v)}
+#     {:twitter=>"someone@twitter.com",
+# :linkedin=>"someone@linkedin.com",
+# :github=>"someone@github.com",
+# :blog=>"someone@blog.com",
+# :profile_quote=>
+#   "\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+# :bio=>
+#   "I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school."}
   end
 
   def self.all
