d8325219391a2d5973381194784ae4265447d366
Stela Capsa <stela.capsa@gmail.com>
2020-01-29 02:33:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,16 +19,7 @@ end
 
 def add_student_attributes(attributes_hash)
     attributes_hash.each{|k,v| self.send(("#{k}="),v)}
-
-#     {:twitter=>"someone@twitter.com",
-# :linkedin=>"someone@linkedin.com",
-# :github=>"someone@github.com",
-# :blog=>"someone@blog.com",
-# :profile_quote=>
-#   "\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
-# :bio=>
-#   "I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary/middle school."}
-  end
+end
 
   def self.all
     @@all
