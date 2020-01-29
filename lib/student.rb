14664a6a9ce9ae353c8f2a46a7d00de4b6dd5864
Stela Capsa <stela.capsa@gmail.com>
2020-01-29 02:12:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each {|a,b| (("#{a}"),b)}
+    attributes_hash.each{|k,v| self.send(("#{k}="),v)}
   end
 
   def self.all
