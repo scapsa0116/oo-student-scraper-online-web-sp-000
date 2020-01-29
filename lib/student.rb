c40c84ba21336ad21a4b6723491843ac5ef84c08
Stela Capsa <stela.capsa@gmail.com>
2020-01-29 02:14:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   def initialize(student_hash)
     student_hash.each {|k,v| self.send(("#{k}="),v)}
-    
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
